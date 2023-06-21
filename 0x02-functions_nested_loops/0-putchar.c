716fd81fb35c634a153db55765b436f6b9fd64b8
chiteri1 <chiteri1000@gmail.com>
2023-06-21 03:27:12 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,35 @@
 #include "main.h"
+
+
+
 /**
- * main - entry point
- * Return: 0
+ * main -Entry point of code,
+ *
+ * Return: Always 0
  */
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
